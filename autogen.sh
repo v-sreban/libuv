699c90a1d733f771f80730a86618d87fe715baeb
v-sreban <v-sreban@microsoft.com>
2015-09-09 14:47:30 +0000

    Update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -3,6 +3,11 @@
 # Copyright (c) 2013, Ben Noordhuis <info@bnoordhuis.nl>
 #
 # Permission to use, copy, modify, and/or distribute this software for any
+#!/bin/sh
+
+# Copyright (c) 2013, Ben Noordhuis <info@bnoordhuis.nl>
+#
+# Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
 # copyright notice and this permission notice appear in all copies.
 #
